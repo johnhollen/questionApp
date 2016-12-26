f7e49976b9767ebfc50a6b1e10cc4345bff61a61
John Hollén <johnhollen982@gmail.com>
2016-12-26 13:52:20 +0100

    Added keyboard avoiding view to add question view

--- src/question/AddQuestion.js
+++ src/question/AddQuestion.js
@@ -1,7 +1,7 @@
 import React, {PropTypes, Component} from 'react';
 import {
     View, Text,
-    TouchableOpacity
+    TouchableOpacity, KeyboardAvoidingView
 } from 'react-native';
 import Input from '../sharedComponents/Input';
 import styles from './AddQuestion.styles.js';
@@ -10,18 +10,20 @@ class AddQuestion extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <KeyboardAvoidingView behavior='position'>
                 <Text style={styles.title}>Ställ din fråga</Text>
-                <Input placeHolder='Skriv din fråga...' />
-                <Text style={[styles.title, styles.provideAnswers]}>Ange dina svar</Text>
-                <Input placeHolder='Första svaret...' />
-                <Input placeHolder='Andra svaret...' />
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity onPress={() => {console.log('Add button press')}}>
-                        <View style={styles.addButton}>
-                            <Text style={styles.addButtonText}>Skapa</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                    <Input placeHolder='Skriv din fråga...' />
+                    <Text style={[styles.title, styles.provideAnswers]}>Ange dina svar</Text>
+                    <Input placeHolder='Första svaret...' />
+                    <Input placeHolder='Andra svaret...' />
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={() => {console.log('Add button press')}}>
+                            <View style={styles.addButton}>
+                                <Text style={styles.addButtonText}>Skapa</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
             </View>
         );
     }
