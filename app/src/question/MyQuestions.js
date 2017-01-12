8830c306b059a93152f408602b18beb7bc7b06e5
John Hollén <johnhollen982@gmail.com>
2017-01-12 19:14:37 +0100

    New view

---
+++ app/src/question/MyQuestions.js
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+import styles from './MyQuestions.styles';
+
+class MyQuestions extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Tjenare</Text>
+            </View>
+        );
+    }
+}
+
+export default MyQuestions;
---
+++ app/src/question/MyQuestions.styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const myQuestionStyles = {
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ff6633',
+        padding: 15
+    }
+};
+
+export default myQuestionStyles;
