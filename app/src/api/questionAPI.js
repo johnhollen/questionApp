b65fc6c301e86bf320f855a3682206665d2082b6
John Hollén <johnhollen982@gmail.com>
2017-01-12 22:33:57 +0100

    Managed to launch app to phone

--- app/src/api/questionAPI.js
+++ app/src/api/questionAPI.js
@@ -4,7 +4,7 @@ console.log('Using Environment >>', process.env.NODE_ENV);
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const BASE_URL = isDevelopment ? 'http://localhost:9000/api' : '';
+const BASE_URL = isDevelopment ? 'http://localhost:9000/api' : 'https://question-app-backend.herokuapp.com/api';
 const RANDOM_QUESTION_URL = `${BASE_URL}/questions/random`;
 
 export const fetchRandomQuestion = () => request(RANDOM_QUESTION_URL);
