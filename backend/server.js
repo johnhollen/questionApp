f0b4006a0eba10410f920259a89e25a7d5356e52
John Hollén <johnhollen982@gmail.com>
2017-01-26 20:05:11 +0100

    Remove comments

--- backend/server.js
+++ backend/server.js
@@ -6,44 +6,25 @@ const serverConfig = require('./serverConfig');
 const morgan = require('morgan');
 const app = express();
 
-//Configure app
 app.use(compression());
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
-
-// parse application/json
 app.use(bodyParser.json());
-
-//Logging
 app.use(morgan('dev'));
 
 // Add headers
 app.use((req, res, next) => {
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
 });
 
 const port = process.env.PORT || 9000;
 
-//Connect to mongodb
 mongoose.Promise = global.Promise;
 mongoose.connect(`${serverConfig.dbUrl}/${serverConfig.dbName}`);
 
-//Load the api modules
 require('./api')(app);
 
 app.get('*', (req, res) => {
