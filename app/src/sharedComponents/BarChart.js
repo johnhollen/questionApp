10bff5c3fa1c88f88f1e976806ee33fd33a87a7e
John Hollén <johnhollen982@gmail.com>
2017-03-05 18:41:44 +0100

    Animating bars in barChart

--- app/src/sharedComponents/BarChart.js
+++ app/src/sharedComponents/BarChart.js
@@ -1,11 +1,42 @@
 import React, {PropTypes, Component} from 'react';
 import {
     View,
-    Text
+    Text,
+    Animated
 } from 'react-native';
 import map from 'lodash/map';
 import reduce from 'lodash/reduce';
 import styles from './BarChart.styles';
+
+class Bar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.widthAnimation = new Animated.Value(0);
+    }
+
+    componentDidMount() {
+        const {width} = this.props;
+
+        Animated.timing(this.widthAnimation, {
+            toValue: width,
+            delay: 500,
+            duration: 500
+        }).start();
+    }
+
+    render() {
+        const {color} = this.props;
+        return (
+            <Animated.View style={[styles.bar, {width: this.widthAnimation, backgroundColor: color}]} />
+        );
+    }
+}
+
+Bar.propTypes = {
+    width: PropTypes.number,
+    color: PropTypes.string
+};
 
 class BarChart extends Component {
     constructor(props) {
@@ -25,6 +56,8 @@ class BarChart extends Component {
 
     renderBars() {
         const {containerWidth} = this.state;
+        if (!containerWidth) return null;
+
         const {data} = this.props;
 
         const sum = reduce(data, (result, current) => result + current.count, 0);
@@ -33,11 +66,12 @@ class BarChart extends Component {
             const percentage = sum === 0 ? 0 : entry.count / sum;
             const widthOfBar = percentage * containerWidth;
             const percentageToPresent = Math.round(percentage * 100);
+
             return (
                 <View style={styles.barContainer} key={`bar_index_${index}`}>
                     <Text style={styles.label}>{entry.label}: {percentageToPresent}%</Text>
                     <View style={styles.barBackground}>
-                        <View style={[styles.bar, {width: widthOfBar, backgroundColor: entry.color}]} />
+                        <Bar width={widthOfBar} color={entry.color} />
                     </View>
                 </View>
             );
